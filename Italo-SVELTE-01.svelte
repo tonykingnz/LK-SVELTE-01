37104f0b38d03d002833a6d3714c2392f6cf77c5
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-11-10 11:19:52 -0300

    Create Italo-SVELTE-01.svelte
    Criar versão aplicação svelte input mais button para renderizar conteúdo informado.
    Recebe os inputs e os alerta na tela.

---
+++ Italo-SVELTE-01.svelte
@@ -0,0 +1,23 @@
+<script>
+	let name;
+	let address;
+	
+	function alerts() {
+		let storeForm = {
+			"name": name,
+			"address": address,
+			};
+		alert(storeForm.name);
+		alert(storeForm.address);
+	};
+</script>
+
+<div>
+	<form method="post" on:submit={alerts}>
+		<label for="Nome">Nome da loja</label>
+		<input type="text" name="nome" bind:value={name} placeholder="Insira o nome:" required>
+		<label for="Endereço">Endereço da loja</label>
+		<input type="text" name="endereço" bind:value={address} placeholder="Insira o endereço:" required>
+		<button type="submit">Enviar formulário</button>
+	</form>
+</div>
